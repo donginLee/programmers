f8bf3f372291daa1b3cb9e8c466f3ebe97836fe3
donginLee <55528388+donginLee@users.noreply.github.com>
2024-01-03 12:57:58 +0900

    feat: 동주 풀이 참고한 새로운 코드 추가

--- algo-kit/hash/2/src/attempt1.cpp
+++ algo-kit/hash/2/src/attempt1.cpp
@@ -1,19 +1,19 @@
 #include <vector>
-#include <map>
+#include <set>
 #include <iostream>
 using namespace std;
+// 동주 설명 참고 후 재작성
 
 int solution(vector<int> nums)
 {
     int answer = 0;
     int n = nums.size();
-    map <int,int> number;
-    
-    for(int i=0;i<nums.size();i++){
-        int count= nums.at(i);
-        if(!number[count])number[count]=1;
-        else number[count]++;
-    }
+    set<int> number(nums.begin(), nums.end());
+    // 각 폰켓몬의 개수가 중요하지 않기 때문에 -> map 을 쓸 필요가 없음
+    // ㄴ set : value 가 필요 없을 때
+    // ㄴ map : value 가 필요할 때     => map을 쓸 때는 value가 반드시 필요한지 생각해야해
+    // set 생성자의 인자가 set의 자료형과 같은 자료형의 벡터의 begin 이터레이터와 end 이터레이터일 경우, 벡터를 좌라락 set에 넣어 줌.
+
     if(n/2>=number.size()){
         answer = number.size();
     }
