68f04b59640d2804b11a29de7081053143b625c8
donginLee <55528388+donginLee@users.noreply.github.com>
2024-01-03 10:16:31 +0900

    feat: 정답 풀이 추가

---
+++ algo-kit/hash/1/src/index.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    map <string,int> table;
+    
+    for(int i=0;i<participant.size();i++){
+        string name = participant.at(i);
+        
+        if(!table[name]) table[name]=1;
+        else table[name]++;
+    }
+    for(int i=0;i<completion.size();i++){
+        string name = completion.at(i);
+        table[name]--;
+    }
+    for(map<string,int>::iterator iter = table.begin();iter!=table.end();iter++){
+        if(iter->second == 1){answer = iter->first; break;}
+    }
+    return answer;
+
+}
