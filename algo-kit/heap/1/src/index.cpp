6b5781e9e876c0eeb5d433e42992dbbc872e13c2
donginLee <55528388+donginLee@users.noreply.github.com>
2024-01-05 10:25:13 +0900

    feat: 더 맵게 풀이 추가

--- algo-kit/heap/1/src/index.cpp
+++ algo-kit/heap/1/src/index.cpp
@@ -1,6 +1,26 @@
+#include <string>
+#include <vector>
+#include <queue>
 #include <iostream>
 using namespace std;
-int main() {
- int a =0;
- cout<< a+1<<"\n"; 
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> q;
+    
+    for(int i=0;i<scoville.size();i++){
+        q.push(scoville.at(i));
+    }
+    while(1){
+        if(q.size()==0) {answer =-1; break;}
+        if(q.size()==1) {if(q.top()<K) answer =-1; break;}
+        int a = q.top();
+        q.pop();
+        if(a>=K)break;
+        int b = q.top();
+        q.pop();
+        q.push(a+2*b);
+        answer++;
+    }
+    return answer;
 }
